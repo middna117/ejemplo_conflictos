5206afe60827ef4be5d18f45e1833b70e2a80485
juan <juandbastidasl@gmail.com>
2026-09-01 10:45:52 -0500

    agregando las clases del proyecto

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a, b, c;
+
+        try {
+            System.out.println("Ingrese un entero A: ");
+            a = Integer.parseInt(br.readLine());
+            System.out.println("Ingrese un entero B: ");
+            b = Integer.parseInt(br.readLine());
+            System.out.println("Ingrese un entero C: ");
+            c = Integer.parseInt(br.readLine());
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
